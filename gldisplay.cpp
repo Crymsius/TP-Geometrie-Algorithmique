8fe3c0ebd66ccc452d7c912ac9f848fd1720ab94
Crymsius <romain.m.mars@gmail.com>
2017-11-14 15:09:53 +0100

    Test crust and switches from shape to mesh with mouse click

--- gldisplay.cpp
+++ gldisplay.cpp
@@ -8,8 +8,15 @@ GLDisplay::GLDisplay(QWidget *parent) :
     QGLWidget(parent),
     _angle(0.0f)
 {
-    mesh.ReadFromOFF("/Users/crymsius/Desktop/Boulot/Master/ID3D/MGA/TP/DisplayMesh/DisplayMesh/queen.off");
-   // mesh.ReadFromOFF("/Users/crymsius/Documents/Xcode/GeomMath/testGeomMath/test.off");
+//    mesh.ReadFromOFF("/Users/crymsius/Desktop/Boulot/Master/ID3D/MGA/TP/DisplayMesh/DisplayMesh/queen.off");
+//        mesh.ReadFromOFF("/Users/crymsius/Documents/Xcode/GeomMath/testGeomMath/cube.off");
+
+//    mesh.ReadFromPoints("/Users/crymsius/Documents/Xcode/GeomMath/testGeomMath/cloud_500.points");
+    mesh.ReadFromPoints("/Users/crymsius/Documents/Xcode/GeomMath/testGeomMath/test_crust/line2.points");
+
+
+    mesh.lawsonAll();
+    shape = mesh.crust();
 }
 
 void GLDisplay::initializeGL()
@@ -28,7 +35,11 @@ void GLDisplay::paintGL()
     glRotatef(_angle, 0.0f, 1.0f, 0.0f);
 //    gasket.draw();
 
-    drawMesh();
+    if (displayswitch) {
+        drawMesh();
+    } else {
+        drawShape();
+    }
 }
 
 void GLDisplay::resizeGL(int w, int h)
@@ -37,7 +48,8 @@ void GLDisplay::resizeGL(int w, int h)
 
     glViewport(0, 0, w, h);
 
-    glOrtho(-1.5f, 1.5f, -1.5f, 1.5f, -1.5f, 1.5f);
+    glOrtho(-1.f, 1.f, -1.f, 1.f, 0.f, 0.f);
+//    glOrtho(-1.5f, 1.5f, -1.5f, 1.5f, -1.5f, 1.5f);
     //glOrtho(0.0f, windowWidth, windowHeight, 0.0f, 0.0f, 1.0f);
 
     glMatrixMode(GL_MODELVIEW);
@@ -45,29 +57,77 @@ void GLDisplay::resizeGL(int w, int h)
 
 void GLDisplay::mouseMoveEvent(QMouseEvent *event)
 {
-    if( event != NULL ) {
-        QPoint position = event->pos();
+//    if( event != NULL ) {
+//        QPoint position = event->pos();
 
-        _angle += (position.x() - _position.x());
+//        _angle += (position.x() - _position.x());
 
-        _position = position;
+//        _position = position;
 
-        updateGL();
-    }
+//        updateGL();
+//    }
 }
 
 void GLDisplay::mousePressEvent(QMouseEvent *event)
 {
-    if( event != NULL )
-        _position = event->pos();
+    if( event != NULL ) {
+        displayswitch = !displayswitch;
+
+//        _position = event->pos();
+//        std::cout << "x : "<< _position.x()/320. - 1. << "  y : " << - (_position.y()/320. - 1.) << std::endl;
+//        Vertex v;
+//        v.position.x = _position.x()/320. - 1.;
+//        v.position.y = - (_position.y()/320. - 1.);
+//        v.position.z = 0;
+//        mesh.inserer_sommet_delaunay_incr(v);
+//        shape = mesh.crust();
+        updateGL();
+    }
+}
+
+void GLDisplay::keyPressEvent(QKeyEvent *event)
+{
+//    if( event != NULL ) {
+//       shape = mesh.crust();
+//       updateGL();
+//    }
+
+}
+
+void GLDisplay::drawShape()
+{
+    const std::vector<Vertex> vertices = mesh._Vertices();
+    const std::vector<std::pair<unsigned, unsigned>> aretes = shape.aretes;
+
+    glBegin(GL_LINES);
+    glColor3f(0.4, 0.4, 0.8);
+    for(unsigned i = 0; i < aretes.size(); ++i)
+    {
+        glVertex3f(vertices[aretes[i].first].position.x,
+                   vertices[aretes[i].first].position.y,
+                   vertices[aretes[i].first].position.z);
+        glVertex3f(vertices[aretes[i].second].position.x,
+                   vertices[aretes[i].second].position.y,
+                   vertices[aretes[i].second].position.z);
+    }
+    glEnd();
 }
 
 void GLDisplay::drawMesh()
 {
-    glBegin(GL_TRIANGLES);
+
+
+//    Vertex v;
+//    v.position.x = - 0.5;
+//    v.position.y = - 0.5;
+//    v.position.z = 0.;
+
+
 
     const std::vector<Vertex> vertices = mesh._Vertices();
     const std::vector<Face> faces = mesh._Faces();
+    glColor3f(1.0, 1.0, 0.0);
+    glBegin(GL_TRIANGLES);
     for(unsigned i = 0; i < faces.size(); ++i)
     {
         for (unsigned j = 0; j < faces[i].nbIndicesPerFace; ++j)
@@ -77,6 +137,64 @@ void GLDisplay::drawMesh()
                        vertices[faces[i].indices[j]].position.z);
         }
     }
-
     glEnd();
+
+    glColor3f(0.0, 0.5, 1.0);
+    for(unsigned i = 0; i < faces.size(); ++i)
+    {
+        glBegin(GL_LINE_LOOP);
+
+
+            glVertex3f(vertices[faces[i].indices[0]].position.x,
+                       vertices[faces[i].indices[0]].position.y,
+                       vertices[faces[i].indices[0]].position.z);
+            glVertex3f(vertices[faces[i].indices[1]].position.x,
+                       vertices[faces[i].indices[1]].position.y,
+                       vertices[faces[i].indices[1]].position.z);
+            glVertex3f(vertices[faces[i].indices[2]].position.x,
+                       vertices[faces[i].indices[2]].position.y,
+                       vertices[faces[i].indices[2]].position.z);
+            glEnd();
+    }
+
+
+//    glBegin(GL_LINES);
+//    glPointSize(12);
+//    glColor3f(1.0, 0.0, 0.0);
+//    for(unsigned i = 0; i < faces.size(); ++i)
+//    {
+//        Cercle circle(faces[i], mesh);
+//        for(unsigned j = 0; j <faces[i].facesAdjacentes.size(); ++j ) {
+//            if (faces[i].facesAdjacentes[j] != -1) {
+//                Cercle circleOp(faces[faces[i].facesAdjacentes[j]], mesh);
+//                glVertex3f(circleOp.centre().x,
+//                           circleOp.centre().y,
+//                           circleOp.centre().z);
+//                glVertex3f(circle.centre().x,
+//                           circle.centre().y,
+//                           circle.centre().z);
+//            }
+//        }
+//    }
+//    glEnd();
+
+
+    glBegin(GL_POINTS);
+    glPointSize(3);
+    glColor3f(1.0, 0.0, 0.0);
+    for(unsigned i = 0; i < faces.size(); ++i)
+    {
+        Cercle circle(faces[i], mesh);
+
+                glVertex3f(circle.centre().x,
+                           circle.centre().y,
+                           circle.centre().z);
+
+
+    }
+    glEnd();
+
+
+
+
 }
--- gldisplay.h
+++ gldisplay.h
@@ -5,6 +5,8 @@
 //#include <gasket.h>
 #include <QMouseEvent>
 #include "/Users/crymsius/Documents/Xcode/GeomMath/Mesh.h"
+#include "/Users/crymsius/Documents/Xcode/GeomMath/Shape.h"
+#include "/Users/crymsius/Documents/Xcode/GeomMath/Cercle.h"
 
 class GLDisplay : public QGLWidget
 {
@@ -21,11 +23,15 @@ public:
 protected:
     virtual void mouseMoveEvent ( QMouseEvent * event );
     virtual void mousePressEvent ( QMouseEvent * event );
+    virtual void keyPressEvent ( QKeyEvent * event );
 
 private:
     void drawMesh();
     Mesh mesh;
 //    Gasket gasket;
+    void drawShape();
+    bool displayswitch=true;
+    Shape shape;
 
     float _angle;
     QPoint _position;
